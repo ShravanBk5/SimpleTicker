2677f7506e6f97f9cae8569247a8b1c9cb69912a
Shravan BK <68052087+ShravanBk5@users.noreply.github.com>
2023-06-03 14:35:43 +0530

    Update ticker.py

--- ticker.py
+++ ticker.py
@@ -2,7 +2,7 @@ import requests
 import datetime as dt
 t = dt.datetime.now()
 def get_price(symbol):
-    url = 'https://api.binance.com/api/v1/ticker/price'
+    url = 'https://api.binance.com/api/v1/ticker/price' #Binance API
     params = {
         'symbol': symbol        
     }
@@ -12,7 +12,7 @@ def get_price(symbol):
 
 while True:
     delta = dt.datetime.now()-t
-    if delta.seconds >= 5:
+    if delta.seconds >= 3: #3 second interval
         print('BTC:'+get_price('BTCUSDT'))
         print('ETH:'+get_price('ETHUSDT'))
         print('------------------')
